dfdd0e8b9a84977a94a24db34ba778d223524763
some113 <100566957+some113@users.noreply.github.com>
2024-05-07 00:03:47 +0700

    Update creamd_av.py

--- preprocess/creamd_av.py
+++ preprocess/creamd_av.py
@@ -7,7 +7,7 @@ import torchaudio
 import numpy as np
 
 dataset = "CREMA-D"
-data_path = '/kaggle/input/'
+data_path = '/kaggle/input/crema-d-1'
 video_dir = os.path.join(data_path, 'face_aligned')
 audio_dir = os.path.join(data_path, 'AudioWAV') # already 16k
 audio_sample_rate = 16000 # expected
